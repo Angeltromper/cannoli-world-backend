2388471c1ec417d52edb55d4e60d5323bc2b4d85
Angelique Tromper <99403100+Angeltromper@users.noreply.github.com>
2023-06-07 00:04:18 +0200

    first commit

---
+++ src/main/java/nl/novi/cannoliworld/CannoliWorldApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.cannoliworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CannoliWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CannoliWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/cannoliworld/CannoliWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.cannoliworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CannoliWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
